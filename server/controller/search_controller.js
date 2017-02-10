b87c73314b51e66ea021ad41e172d706842a4b99
佑佑科技 <apple2@ioioinfo.com>
2017-02-10 19:22:05 +0800

    product show

--- server/controller/search_controller.js
+++ server/controller/search_controller.js
@@ -18,11 +18,17 @@ var find_sorts = function(cb){
 	var url = "http://127.0.0.1:8050/search_sorts";
 	do_get_method(url,cb);
 };
+//查询商品信息
+var find_product_info = function(id,cb){
+	var url = "http://127.0.0.1:8080/mp_product_info?product_id="+id;
+	do_get_method(url,cb);
+};
 exports.register = function(server, options, next){
 	server.route([
+		//分类
 		{
 			method: 'GET',
-			path: '/search_result',
+			path: '/sort',
 			handler: function(request, reply){
 				find_sorts(function(err, content){
 					if (!err) {
@@ -40,12 +46,143 @@ exports.register = function(server, options, next){
 						}
 						console.log("level_map: "+JSON.stringify(level_map));
 						var sorts_infos = JSON.stringify(level_map);
-						return reply.view("web-list",{"sorts":level_map,"sorts_infos":sorts_infos})
+						return reply.view("sort",{"sorts":level_map,"sorts_infos":sorts_infos});
 					}else {
 					}
 				});
 			}
 		},
+		//查询
+		{
+			method: 'GET',
+			path: '/search',
+			handler: function(request, reply){
+				return reply.view("search");
+			}
+		},
+		//查询搜索
+		{
+			method: 'GET',
+			path: '/search_area',
+			handler: function(request, reply){
+				return reply.view("search_area");
+			}
+		},
+		//产品展示
+		{
+			method: 'GET',
+			path: '/product_show',
+			handler: function(request, reply){
+				var product_id = request.query.product_id;
+				find_product_info(product_id, function(err, content){
+					console.log(content);
+					if (!err) {
+						return reply.view("product_show",{"product":content.product})
+					}else {
+
+					}
+				});
+				// return reply.view("product_show");
+			}
+		},
+		//产品展示
+		{
+			method: 'GET',
+			path: '/shopping_cart',
+			handler: function(request, reply){
+				return reply.view("shopping_cart");
+			}
+		},
+		//产品展示
+		{
+			method: 'GET',
+			path: '/order_pay',
+			handler: function(request, reply){
+				return reply.view("order_pay");
+			}
+		},
+		//个人中心
+		{
+			method: 'GET',
+			path: '/person_center',
+			handler: function(request, reply){
+				return reply.view("person_center");
+			}
+		},
+		//订单中心
+		{
+			method: 'GET',
+			path: '/order_center',
+			handler: function(request, reply){
+				return reply.view("order_center");
+			}
+		},
+		//订单详情
+		{
+			method: 'GET',
+			path: '/order_detail',
+			handler: function(request, reply){
+				return reply.view("order_detail");
+			}
+		},
+		//微信注册
+		{
+			method: 'GET',
+			path: '/chat_register',
+			handler: function(request, reply){
+				return reply.view("chat_register");
+			}
+		},
+		//微信注册存在
+		{
+			method: 'GET',
+			path: '/exist_register',
+			handler: function(request, reply){
+				return reply.view("exist_register");
+			}
+		},
+		//新账号设置密码
+		{
+			method: 'GET',
+			path: '/set_password',
+			handler: function(request, reply){
+				return reply.view("set_password");
+			}
+		},
+		//微信账号登入
+		{
+			method: 'GET',
+			path: '/chat_login',
+			handler: function(request, reply){
+				return reply.view("chat_login");
+			}
+		},
+		//微信个人中心
+		{
+			method: 'GET',
+			path: '/chat_person_center',
+			handler: function(request, reply){
+				return reply.view("chat_person_center");
+			}
+		},
+		//微信充值中心
+		{
+			method: 'GET',
+			path: '/chongzhi_center',
+			handler: function(request, reply){
+				return reply.view("chongzhi_center");
+			}
+		},
+		//微信小票
+		{
+			method: 'GET',
+			path: '/chat_receipt',
+			handler: function(request, reply){
+				return reply.view("chat_receipt");
+			}
+		},
+
+
 
     ]);
 
