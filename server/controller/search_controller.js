47cca85bd02078238db3579521ae2113dffbdf23
佑佑科技 <apple2@ioioinfo.com>
2017-02-13 19:42:28 +0800

    页面

--- server/controller/search_controller.js
+++ server/controller/search_controller.js
@@ -1,21 +1,47 @@
 // Base routes for item..
-
 const uu_request = require('../utils/uu_request');
-
+var eventproxy = require('eventproxy');
 var do_get_method = function(url,cb){
 	uu_request.get(url, function(err, response, body){
 		if (!err && response.statusCode === 200) {
 			var content = JSON.parse(body);
-			cb(false, content);
+			do_result(false, content, cb);
 		} else {
 			cb(true, null);
 		}
 	});
 };
-
+//所有post调用接口方法
+var do_post_method = function(url,data,cb){
+	uu_request.request(url, data, function(err, response, body) {
+		console.log(body);
+		if (!err && response.statusCode === 200) {
+			do_result(false, body, cb);
+		} else {
+			cb(true,null);
+		}
+	});
+};
 //查找商品分类
 var find_sorts = function(cb){
 	var url = "http://127.0.0.1:8050/search_sorts";
+	do_get_method(url,cb);
+};
+//cooke person
+var get_cookie_person = function(request){
+	var person_id;
+	if (request.state && request.state.cookie) {
+		state = request.state.cookie;
+		if (state.person_id_mb) {
+			person_id = state.person_id_mb;
+		}
+	}
+	return person_id;
+};
+//收藏
+var find_favorite = function(product_id,person_id,cb){
+	var url = "http://139.196.148.40:18003/favorite/get_by_person_and_product?product_id=";
+	url = url + product_id +"&person_id=" + person_id;
 	do_get_method(url,cb);
 };
 //查询商品信息
@@ -23,8 +49,47 @@ var find_product_info = function(id,cb){
 	var url = "http://127.0.0.1:8080/mp_product_info?product_id="+id;
 	do_get_method(url,cb);
 };
+//处理结果
+var do_result = function(err,result,cb){
+	if (!err) {
+		if (result.success) {
+			cb(false,result);
+		}else {
+			cb(true,result);
+		}
+	}else {
+		cb(true,null);
+	}
+};
+//更新收藏
+var update_favorite = function(data,cb){
+	var url = "http://139.196.148.40:18003/favorite/save_favorite";
+	do_post_method(url,data,cb);
+};
+//根据personid找头像
+var find_persons = function(persons, cb){
+	var platform_id = "ec_shopping";
+	var url = "http://139.196.148.40:18003/get_person_avatar?person_ids=";
+	url = url + persons + "&scope_code=" +platform_id;
+	do_get_method(url,cb);
+};
+//根据personid找vip
+var find_personsVip = function(persons, cb){
+	var platform_id = "ec_shopping"
+	var url = "http://139.196.148.40:18003/vip/list_by_scope_persons?person_ids=";
+	url = url + persons + "&scope_code=" +platform_id;
+	do_get_method(url,cb);
+};
 exports.register = function(server, options, next){
 	server.route([
+		//desc,需要服务器地址
+		{
+			method: 'GET',
+			path: '/desc',
+			handler: function(request, reply){
+				return reply({"success":"ok","server":"ec_search server, ec_interaction server, ec_shopping_cart server"});
+			}
+		},
 		//分类
 		{
 			method: 'GET',
@@ -74,18 +139,62 @@ exports.register = function(server, options, next){
 			path: '/product_show',
 			handler: function(request, reply){
 				var product_id = request.query.product_id;
+				var person_id = get_cookie_person(request);
+				if (!person_id) {
+					person_id = 1;
+				}
+				var is_active = 0;
 				find_product_info(product_id, function(err, content){
-					console.log(content);
 					if (!err) {
-						return reply.view("product_show",{"product":content.product})
+						find_favorite(product_id,person_id,function(err,result){
+							if (!err) {
+								if (result.row && result.row.is_active) {
+									is_active = result.row.is_active;
+								}
+								return reply.view("product_show",{"product":content.product,"is_active":is_active});
+							}else {
+
+							}
+						});
+
 					}else {
 
 					}
+
+
 				});
 				// return reply.view("product_show");
 			}
 		},
-		//产品展示
+		//修改收藏
+		{
+			method: 'POST',
+			path: '/update_favorite',
+			handler: function(request, reply){
+				var person_id = get_cookie_person(request);
+				if (!person_id) {
+					person_id = 1;
+				}
+				var is_active = 0
+				if (request.payload.is_active == 0) {
+					var is_active = 1;
+				}
+				var data = {
+					person_id :person_id,
+					product_id : request.payload.product_id,
+					is_active : is_active
+				};
+				update_favorite(data,function(err,result){
+					if (!err) {
+						return reply({"success":true});
+					}else {
+							return reply({"success":false});
+					}
+				});
+			}
+		},
+
+		//购物车
 		{
 			method: 'GET',
 			path: '/shopping_cart',
@@ -93,7 +202,7 @@ exports.register = function(server, options, next){
 				return reply.view("shopping_cart");
 			}
 		},
-		//产品展示
+		//订单
 		{
 			method: 'GET',
 			path: '/order_pay',
@@ -106,7 +215,46 @@ exports.register = function(server, options, next){
 			method: 'GET',
 			path: '/person_center',
 			handler: function(request, reply){
-				return reply.view("person_center");
+				var person_id = get_cookie_person(request);
+				if (!person_id) {
+					person_id = 1;
+				}
+				var ep =  eventproxy.create("persons","personsVip",
+					function(persons,personsVip){
+					return reply.view("person_center",{"success":true,"persons":persons,"personsVip":personsVip});
+				});
+				find_persons(person_id, function(err, content){
+					if (!err) {
+						var persons = content.rows;
+						ep.emit("persons", persons);
+					}else {
+						ep.emit("persons", []);
+					}
+				});
+				find_personsVip(person_id, function(err, content){
+					if (!err) {
+						var personsVip = content.rows;
+						ep.emit("personsVip", personsVip);
+					}else {
+						ep.emit("personsVip", []);
+					}
+				});
+			}
+		},
+		//退出loginout
+		{
+			method: 'GET',
+			path: '/logout',
+			handler: function(request, reply){
+				return reply({"success":true}).state('cookie', {});
+			}
+		},
+		//个人设置
+		{
+			method: 'GET',
+			path: '/configure',
+			handler: function(request, reply){
+				return reply.view("configure");
 			}
 		},
 		//订单中心
@@ -115,6 +263,86 @@ exports.register = function(server, options, next){
 			path: '/order_center',
 			handler: function(request, reply){
 				return reply.view("order_center");
+			}
+		},
+		//查看物流
+		{
+			method: 'GET',
+			path: '/check_logistic',
+			handler: function(request, reply){
+				return reply.view("check_logistic");
+			}
+		},
+		//地址管理
+		{
+			method: 'GET',
+			path: '/address_management',
+			handler: function(request, reply){
+				return reply.view("address_management");
+			}
+		},
+		//新增地址
+		{
+			method: 'GET',
+			path: '/add_address',
+			handler: function(request, reply){
+				return reply.view("add_address");
+			}
+		},
+		//新增地址
+		{
+			method: 'GET',
+			path: '/edit_address',
+			handler: function(request, reply){
+				return reply.view("edit_address");
+			}
+		},
+		//新增地址
+		{
+			method: 'GET',
+			path: '/favorite_list',
+			handler: function(request, reply){
+				return reply.view("favorite_list");
+			}
+		},
+		//代付款
+		{
+			method: 'GET',
+			path: '/pre_pay',
+			handler: function(request, reply){
+				return reply.view("pre_pay");
+			}
+		},
+		//代发货
+		{
+			method: 'GET',
+			path: '/pre_send',
+			handler: function(request, reply){
+				return reply.view("pre_send");
+			}
+		},
+		//代收货
+		{
+			method: 'GET',
+			path: '/pre_receive',
+			handler: function(request, reply){
+				return reply.view("pre_receive");
+			}
+		},
+		//待评价
+		{
+			method: 'GET',
+			path: '/pre_comment',
+			handler: function(request, reply){
+				return reply.view("pre_comment");
+			}
+		},
+		//售后
+		{
+			method: 'GET',
+			path: '/after_sale',
+			handler: function(request, reply){
+				return reply.view("after_sale");
 			}
 		},
 		//订单详情
