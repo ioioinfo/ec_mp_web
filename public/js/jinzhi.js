1466ca00025470c087b6c1420f952c683a2d5aa8
佑佑科技开发 <apple3@ioioinfo.com>
2017-04-12 19:11:19 +0800

    1

---
+++ public/js/jinzhi.js
@@ -0,0 +1,13 @@
+$(function() {
+    function hengshuping(){
+        if(window.orientation==180||window.orientation==0){
+            $(".hengPing").hide().siblings().not("script").show();
+        }
+        if(window.orientation==90||window.orientation==-90){
+            $(".hengPing").show().siblings().not("script").hide();
+        }
+    }
+
+    $("body").append('<div class="hengPing"><span>请勿横屏哦</span></div>');
+    window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", hengshuping, false);
+});
