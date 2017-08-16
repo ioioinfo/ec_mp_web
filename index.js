93ee0e0a2fc6a751df690d8c3af98a4e0928fbba
oopfuture <oopfuture@gmail.com>
2017-08-16 18:09:19 +0800

    日志

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ server.register([
     {
         register: require("good"),
         options: {
-            ops: {interval: 5000},
+            ops: false,
             reporters: {
                 myConsoleReporter: [{
                     module: 'good-console'
