b7a858f6c62cbdf0092a0695fafecc9aee4363a1
oopfuture <oopfuture@gmail.com>
2017-08-16 20:08:24 +0800

    产品版本

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,11 @@ module.exports = {
         ]
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
         new webpack.optimize.UglifyJsPlugin({
             minimize: true,
             compress: { warnings: false }
