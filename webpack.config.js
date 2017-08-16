dc80cc1b8e92bfa558a46f404901973a0fb77716
oopfuture <oopfuture@gmail.com>
2017-08-16 20:03:37 +0800

    压缩

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,9 @@ module.exports = {
         ]
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({minimize: true})
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: { warnings: false }
+        })
     ]
 };
